9de9e78f9998b32d8a91acf154dd2272f161f152
Mojang <dev@mojang.com>
2019-12-09 13:13:38 +0000

    1.15

--- minecraft/src/net/minecraft/DetectedVersion.java
+++ minecraft/src/net/minecraft/DetectedVersion.java
@@ -26,10 +26,10 @@ public class DetectedVersion implements GameVersion {
 
 	public DetectedVersion() {
 		this.id = UUID.randomUUID().toString().replaceAll("-", "");
-		this.name = "1.15-pre7";
-		this.stable = false;
-		this.worldVersion = 2224;
-		this.protocolVersion = 572;
+		this.name = "1.15";
+		this.stable = true;
+		this.worldVersion = 2225;
+		this.protocolVersion = 573;
 		this.packVersion = 5;
 		this.buildTime = new Date();
 		this.releaseTarget = "1.15";
