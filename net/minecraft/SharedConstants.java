2862f24af6041128735f39996b673c39ce495d2d
NikitaCartes <nikich98@yandex.com>
2022-02-28 10:42:45 +0000

    1.18.2

--- net/minecraft/SharedConstants.java
+++ net/minecraft/SharedConstants.java
@@ -14,13 +14,13 @@ import org.jetbrains.annotations.Nullable;
 
 public class SharedConstants {
     @Deprecated
-    public static final boolean SNAPSHOT = true;
+    public static final boolean SNAPSHOT = false;
     @Deprecated
-    public static final int WORLD_VERSION = 2974;
+    public static final int WORLD_VERSION = 2975;
     @Deprecated
     public static final String SERIES = "main";
     @Deprecated
-    public static final String VERSION_STRING = "1.18.2-rc1";
+    public static final String VERSION_STRING = "1.18.2";
     @Deprecated
     public static final String RELEASE_TARGET = "1.18.2";
     @Deprecated
@@ -158,7 +158,7 @@ public class SharedConstants {
     }
 
     public static int getProtocolVersion() {
-        return 0x40000049;
+        return 758;
     }
 
     public static boolean debugVoidTerrain(ChunkPos chunkPos) {
